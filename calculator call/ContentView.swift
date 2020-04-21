1d2059669010db0948dc418597ab61694dc755ed
ziyugao <gaoziyu0125@gmail.com>
2020-04-21 12:49:57 -0400

    Initial Commit

---
+++ calculator call/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator call
+//
+//  Created by Ziyu Gao on 4/21/20.
+//  Copyright © 2020 Ziyu Gao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
